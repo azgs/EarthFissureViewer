33639f62511d4c5ff7539530f9f1852a6c5f62df
asonnenschein <adrian.sonnenschein@gmail.com>
2013-11-08 13:23:16 -0700

    switched to mapbox basemap

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 var app = {
   map: L.map('map', {center: [32.3, -111], zoom: 8, minZoom: 8, maxZoom:14}),
   layers: {
-	  baseLayer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //'http://a.tiles.mapbox.com/v3/azgs.map-qc1pcpws/{z}/{x}/{y}.png', {
+	  baseLayer: L.tileLayer('http://a.tiles.mapbox.com/v3/azgs.map-qc1pcpws/{z}/{x}/{y}.png', { //'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    attribution: '<a href="https://www.mapbox.com/about/maps/">Terms & Feedback</a>',
 	    detectRetina: true
 	  }),
@@ -45,33 +45,33 @@ var app = {
 function onEachFeature(feature, layer) {
 	var pdf = feature.properties.Pdf.split(','),
 	    label = feature.properties.Label,
-			bbox = layer.getBounds().toBBoxString(),
+		bbox = layer.getBounds().toBBoxString(),
 
-			lastOne = pdf[pdf.length - 1],
-			preview = 'assets/' + lastOne + '.png',
+		lastOne = pdf[pdf.length - 1],
+		preview = 'assets/' + lastOne + '.png',
 
-			links = _.map(pdf, function (item) {
-				var path = 'assets/' + item + '.pdf';
-				return '<div class="alert alert-info"><a href="' + path + '">' + item + '</a></div>';
-			}).join('');
+		links = _.map(pdf, function (item) {
+			var path = 'assets/' + item + '.pdf';
+			return '<div class="alert alert-info"><a href="' + path + '">' + item + '</a></div>';
+		}).join('');
 
-			urlExists = function(url) {
-				var http = new XMLHttpRequest();
-				http.open('HEAD', url, false);
-				http.send();
-				if (http.status === 200) {
-					return links;
-				} else {
-					return '<div class="alert alert-danger">Not Available</div>';
-				}
+		urlCheck = function(url) {
+			var http = new XMLHttpRequest();
+			http.open('HEAD', url, false);
+			http.send();
+			if (http.status === 200) {
+				return links;
+			} else {
+				return '<div class="alert alert-danger">Not Available</div>';
 			}
+		}
 
 	var html = '<div class="title"><h4>' + label + ' Study Area</h4></div>';
 		html += '<table><tr>';
 		html += '<td style="width:250px;height:' + app.imgHeights[lastOne] + 'px;"><img src="' + preview + '" onerror=this.style.display="none"; /></td>';
 		html += '<td style="padding-left:10px;">';
 		html += '<h5>Downloadable Maps:</h5>';
-		html += '<div class=download>' + urlExists(preview) + '</div>';
+		html += '<div class=download>' + urlCheck(preview) + '</div>';
 		html += '<button type="button" onclick="doZoom(' + bbox + ')" class="btn btn-success">Zoom to this study area</button>';
 		html += '</td></tr></table>';
 
