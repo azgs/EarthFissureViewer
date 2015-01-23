e934209d6ed2a39c4c5aa88d554eed9b875f7c6c
Lbookman <laura.bookman@azgs.az.gov>
2015-01-23 15:20:16 -0700

    added new fissures and study areas
    removed TileMill and fissures are being pulled from geojson

--- scripts/map.js
+++ scripts/map.js
@@ -5,7 +5,15 @@ var app = {
 	    attribution: '<a href="https://www.mapbox.com/about/maps/">Terms & Feedback</a>',
 	    detectRetina: true
 	  }),
-	  fissuresLayer: L.tileLayer('http://{s}.tiles.usgin.org/fissures/{z}/{x}/{y}.png'),
+	  fissuresLayer: L.geoJson(null, {
+      style: function(feature) {
+        switch (feature.properties.Label) {
+          case 'Reported, unconfirmed earth fissure': return {color: 'green', "weight": 2, dashArray: "6,5"};
+          case 'Discontinuous earth fissure':   return {color: 'red', "weight": 2};
+          case 'Continuous earth fissure':   return {color: 'black', "weight": 2};
+        }
+      }
+    }),
 	  studyAreas: L.geoJson(null, {
 	  	style: {
 	  		'color': 'rgb(54,204,255)',
@@ -19,31 +27,6 @@ var app = {
 	  	onEachFeature: photographize
 	  })
 */
-  },
-  imgHeights: {
-  	'luke': 188,
-	  'picacho-3': 204,
-	  'harquahala': 210,
-	  'greene-wash': 240,
-	  'white-horse-pass': 247,
-	  'santa-rosa-wash': 119,
-	  'toltec-buttes': 206,
-	  'scottsdale': 143,
-	  'apache-junction': 238,
-	  'chandler-heights': 139,
-	  'petes-corner': 211,
-	  'wintersburg': 231,
-	  'heaton': 116,
-	  'tator-hills': 108,
-	  'signal-peak': 217,
-	  'friendly-corner': 217,
-	  'sacaton-butte': 245,
-	  'three-sisters-butte': 225,
-	  'dragoon-road': 234,
-	  'mesa': 135,
-	  'croton-springs': 226,
-	  'elfrida': 226,
-	  'bowie-san-simon': 210
   }
 };
 
@@ -73,14 +56,14 @@ function onEachFeature(feature, layer) {
 
 	var html = '<div class="title"><h4>' + label + ' Study Area</h4></div>';
 		html += '<table><tr>';
-		html += '<td style="width:250px;height:' + app.imgHeights[lastOne] + 'px;"><img src="' + preview + '" onerror=this.style.display="none"; /></td>';
+		html += '<td width=200><img width=200 src="' + preview + '" onerror=this.style.display="none"; /></td>';
 		html += '<td style="padding-left:10px;">';
 		html += '<h5>Downloadable Maps:</h5>';
 		html += '<div class=download>' + urlCheck(preview) + '</div>';
 		html += '<button type="button" onclick="doZoom(' + bbox + ')" class="btn btn-success">Zoom to this study area</button>';
 		html += '</td></tr></table>';
 
-	layer.bindPopup(html, { minWidth: 430 });
+	layer.bindPopup(html, { minWidth: 400 });
 }
 /*
 function photographize(feature, layer) {
@@ -131,15 +114,20 @@ app.layers.fissuresLayer.addTo(app.map);
 app.layers.studyAreas.addTo(app.map);
 L.geocoderControl().addTo(app.map);
 
+d3.json('data/earth_fissures.json', function (err, data) {
+	if (err) return console.log(err);
+	app.layers.fissuresLayer.addData(data);
+});
+
 d3.json('data/earth_fissure_study_areas.json', function (err, data) {
 	if (err) return console.log(err);
 	app.layers.studyAreas.addData(data);
 });
 
-d3.json('data/earth_fissure_photo_areas.json', function (err, data) {
+/*d3.json('data/earth_fissure_photo_areas.json', function (err, data) {
 	if (err) return console.log(err);
 	app.layers.photoAreas.addData(data);
-})
+}) */
 
 // Add click effect to the legend toggler
 d3.select('#toggle-info').on('click', function () {
